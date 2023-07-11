097db8e945fc0545cd5290781d61e4ddc7361969
zxuu <z18255713582@outlook.com>
2023-07-11 10:11:07 +0800

    first commit

---
+++ rnn.py
@@ -0,0 +1,44 @@
+from torch import nn
+import torch
+
+class RNN_Cell(nn.Module):
+    def __init__(self, in_dim, hidden_dim):
+        super(RNN_Cell, self).__init__()
+        self.Wx = nn.Parameter(torch.rand(in_dim, hidden_dim))
+        self.Wh = nn.Parameter(torch.rand(hidden_dim, hidden_dim))
+        self.b = nn.Parameter(torch.rand(1, hidden_dim))
+
+    def forward(self, x, h_1):
+        h = torch.tanh(torch.matmul(x, self.Wx) + torch.matmul(h_1, self.Wh) + self.b)
+        return h
+
+class RNN(nn.Module):
+    def __init__(self, in_dim, hidden_dim):
+        super(RNN, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.rnn_cell = RNN_Cell(in_dim, hidden_dim)
+
+    def forward(self, x):
+        """
+        x: [seq_len, batch_size, in_dim]
+        """
+        outs = []
+        h = None
+        for seq_x in x:
+            # seq_x: [batch, in_dim]
+            if h is None:
+                h = torch.randn(x.shape[1], self.hidden_dim)    # [batch, hidden_dim]
+            h = self.rnn_cell(seq_x, h)
+            outs.append(torch.unsqueeze(h,0))
+        outs = torch.cat(outs)
+        return outs, h
+
+
+if __name__ == '__main__':
+    batch_size, seq_lens, in_dim, out_dim = 24, 7, 12, 6
+    rnn = RNN(in_dim, out_dim)
+    x = torch.randn(seq_lens, batch_size, in_dim)
+    outs, h = rnn(x)
+
+    print(outs.shape)
+    print(h.shape)
---
+++ rnn_pytorch.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+
+# rnn = nn.RNN(input_size=12, hidden_size=6, batch_first=True)    # 默认为false
+rnn = nn.RNN(input_size=12, hidden_size=6)
+
+input  = torch.randn(24, 5, 12)
+outputs, hn = rnn(input)
+
+print(outputs.size())
+print(hn.size())
